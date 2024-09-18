736b9e0898a9dd884a41c1ee782f9d2c2b002ebc
Uri Baynesay <uri552@gmail.com>
2024-09-18 13:51:58 +0300

    fix lint issue

--- app/(main)/chats/page.tsx
+++ app/(main)/chats/page.tsx
@@ -1,7 +1,9 @@
+import { redirect } from "next/navigation"
 import { getProfile } from "../profile/db"
 
 const ChatsPage = async () => {
   const profile = await getProfile()
+  if (!profile) redirect("/sign-in")
   return (
     <section className="grow">
       <h2>Hello {profile?.name}</h2>
