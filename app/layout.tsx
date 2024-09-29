9e284982137baeff393844cb5d2f00e6ec25ddbe
Uri Baynesay <uri552@gmail.com>
2024-09-29 16:31:48 +0300

    add metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,10 @@ import "./globals.css"
 
 import { Inter } from "next/font/google"
 
-const inter = Inter({ subsets: ["latin"] ,weight:["100","200","300","400","500","600","700","800","900"]})
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+})
 
 export const generateMetadata = () => {
   return {
@@ -17,6 +20,16 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={`${inter.className} antialiased`}>
+      <head>
+        <meta property="og:image" content="<generated>" />
+        <meta property="og:image:type" content="<generated>" />
+        <meta property="og:image:width" content="<generated>" />
+        <meta property="og:image:height" content="<generated>" />
+        <meta
+          name="description"
+          content="Welcome to the Whatsup project! This is a social messaging application designed to facilitate seamless communication between users."
+        />
+      </head>
       <body className="bg-background text-foreground h-dvh m-0 p-0 box-border text-lg font-medium">
         {children}
       </body>
